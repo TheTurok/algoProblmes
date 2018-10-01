2966b2e958df9846f481a12e621a3563a8555aca
Timothy Choi <TurokChoi@gmail.com>
2018-10-01 00:20:18 -0700

    yay one done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for(let char of str){
+    if(!charMap[char])
+      charMap[char] = 1;
+    else {
+      charMap[char]++;
+    }
+  }
+
+  let maxChar = '';
+  let value = 0;
+  for(let char in charMap){
+    if(charMap[char] > value){
+      maxChar = char;
+      value = charMap[char];
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
