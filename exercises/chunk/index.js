6235e633557f071b7a36befa77f0b10d4dcad96c
Timothy Choi <TurokChoi@gmail.com>
2018-10-01 19:17:58 -0700

    finished chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunk = [];
+  const chunks = [];
+
+  for( i = 0; i < array.length; i++){
+    if(i % size == 0 && i != 0){
+      chunks.push(chunk);
+      chunk = [];
+    }
+    chunk.push(array[i]);
+
+  }
+
+  if(chunk)
+    chunks.push(chunk);
+
+  return chunks;
+
+}
 
 module.exports = chunk;
