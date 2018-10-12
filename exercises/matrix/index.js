3e4423e04f5b0c66b4def6439ac5efe81fbefa5e
Timothy Choi <TurokChoi@gmail.com>
2018-10-11 17:56:08 -0700

    spiral finished

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,47 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const spiral = [n,n];
+  let startCol = 0;
+  let startRow = 0;
+  let endCol = n-1;
+  let endRow = n-1;   //starting values
+  let currCol = 0;
+  let currRow = 0;
+
+  for(let i = 0; i < n ; i++){
+    spiral.push([]);
+  }
+
+  let count = 1;
+
+  while(startCol <= endCol && startRow <= endRow){ //left to right
+    console.log("r: " + currRow);
+    console.log("c: " + currCol);
+    while(currCol <= endCol){
+      spiral[currRow][currCol] = count;
+      count++; currCol++;
+    }
+    startRow++; currRow++; currCol--;
+    while(currRow <= endRow){ //top to bottom
+      spiral[currRow][currCol] = count;
+      count++; currRow++;
+    }
+    endCol--; currCol--; currRow--;
+    while(currCol >= startCol){ //right to left
+      spiral[currRow][currCol] = count;
+      count++; currCol--;
+    }
+    endRow--; currRow--; currCol++;
+    while(currRow >= startRow){ //right to left
+      spiral[currRow][currCol] = count;
+      count++; currRow--;
+    }
+    startCol++; currCol++; currRow++;
+  }
+
+  return spiral;
+}
 
 module.exports = matrix;
