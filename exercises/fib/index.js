3c03d630864b431bc7b8aa5a8d15eeb5e9246d74
Timothy Choi <TurokChoi@gmail.com>
2018-10-14 20:43:08 -0700

    finished more exercises

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -3,11 +3,36 @@
 // The fibonacci series is an ordering of numbers where
 // each number is the sum of the preceeding two.
 // For example, the sequence
-//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+//  [1, 1, 2, 3, 5, 8, 13, 21, 34]
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn){
+  const cache = {};
+  return function(...args){
+    if(cache[args]){
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if(n === 0)
+    return 0;
+
+  if(n === 1)
+    return 1;
+
+  return fib(n-1) + fib(n-2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
