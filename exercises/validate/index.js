4495864a6c23326e2de4a82c11ce2f14dbead9d6
Timothy Choi <TurokChoi@gmail.com>
2018-10-19 17:58:40 -0700

    valid bst

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,25 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  let validL = true;
+  let validR = true;
+
+  if(node.left && (node.left.data > node.data ||
+      (min && node.left.data < min)))
+    return false
+
+  if(node.right && (node.right.data < node.data ||
+      (max && node.right.data > max)))
+    return false
+
+
+  if(node.left)
+    validL = validate(node.left, min, node.data);
+  if(node.right)
+    validR = validate(node.right, node.data, max);
+
+  return validL && validR;
+}
 
 module.exports = validate;
