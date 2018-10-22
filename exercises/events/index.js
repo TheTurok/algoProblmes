2f46ccfe0c84fd684ed2e1d72ca41353450a8f5e
Timothy Choi <TurokChoi@gmail.com>
2018-10-21 19:51:55 -0700

    yuss

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,32 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor(){
+    this.eventList = {};
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if(!this.eventList[eventName])
+      this.eventList[eventName] = new Array();
+
+    this.eventList[eventName].push(callback);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if(this.eventList[eventName])
+      for(let cb of this.eventList[eventName]){
+        cb();
+      }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.eventList[eventName];
+  }
 }
 
 module.exports = Events;
